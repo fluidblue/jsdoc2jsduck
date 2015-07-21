0160b1de0f9c3d5b79f164e9c8c3a09ad50671ff
fluidblue <fluidblue@users.noreply.github.com>
2015-07-21 11:31:55 +0200

    processDocTree

--- jsdoc2jsduck.js
+++ jsdoc2jsduck.js
@@ -124,29 +124,78 @@ function isAllowedScope(scope) {
 	return filter.allowedScopes.indexOf(scope) > -1;
 }
 
+function isAllowedPackage(scope) {
+	if (filter === null || !filter.allowedPackages) {
+		return true;
+	}
+	// TODO
+	return true;
+}
+
+function getKindPrecedence(kind) {
+	switch (kind) {
+		case 'package': return 0;
+
+		case 'class': return 0;
+		case 'interface': return 0;
+		case 'function': return 0;
+		
+		case 'member': return 0;
+		case 'constant': return 0;
+
+		case 'event': return 0;
+		case 'external': return 0;
+		case 'file': return 0;
+		
+		
+		case 'mixin': return 0;
+		case 'module': return 0;
+		case 'namespace': return 0;
+		
+		case 'param': return 0;
+		case 'typedef': return 0;
+		default: return 0;
+	}
+}
+
 function processDocTree(docTree, parentKind) {
 	var output = "";
 	if (docTree.jsdocs !== null) {
-		var allowedJSDocs = [];
+		var allowedJSDocs = {};
 		for (var i = 0; i < docTree.jsdocs.length; i++) {
-			if (isAllowedChild(parentKind, docTree.jsdocs[i].kind)) {
-				allowedJSDocs.push(docTree.jsdocs[i]);
+			var jsdoc = docTree.jsdocs[i];
+			if (!isAllowedChild(parentKind, jsdoc.kind)) {
+				//console.log("Filtered by allowedChildren: " + jsdoc.longname);
+				continue;
+			}
+			if (!isAllowedPackage(jsdoc.longname)) {
+				//console.log("Filtered by allowedPackages: " + jsdoc.longname);
+				continue;
+			}
+			if (allowedJSDocs.hasOwnProperty(jsdoc.longname)) {
+				if (allowedJSDocs[jsdoc.longname].inheritdoc && !jsdoc.inheritdoc) {
+					allowedJSDocs[jsdoc.longname] = jsdoc;
+				} else if (!allowedJSDocs[jsdoc.longname].inheritdoc && jsdoc.inheritdoc) {
+					// Discard current jsdoc
+				} else {
+					console.log("Warning: Multiple JSDocs found for " + jsdoc.longname);
+					// TODO: Decide by kind precedence
+				}
 			} else {
-				//console.log("Filtered by allowedChildren: " + docTree.jsdocs[i].longname);
+				allowedJSDocs[jsdoc.longname] = jsdoc;
 			}
 		}
-		// TODO: Make allowedJSDocs an object; check for .inheritdoc
-		for (var i = 0; i < allowedJSDocs.length; i++) {
-			output += processJSDoc(allowedJSDocs[i]);
-
-			// TODO: Check
-			parentKind = allowedJSDocs[i].kind;
-
-			if (i >= 1) {
-				console.log("Warning: Multiple JSDocs found for " + allowedJSDocs[i].longname);
-				// TODO
-				//return;
+		if (Object.keys(allowedJSDocs).length > 1) {
+			console.log("Warning: Multiple JSDocs found for path " + getPath(jsdoc.longname));
+		}
+		// TODO: Check for precedence, check for "memberof"
+		for (var longname in allowedJSDocs) {
+			if (!allowedJSDocs.hasOwnProperty(longname)) {
+				continue;
 			}
+
+			output += processJSDoc(allowedJSDocs[longname]);
+			parentKind = allowedJSDocs[longname].kind;
 		}
 	}
 	if (parentKind === "root") {
